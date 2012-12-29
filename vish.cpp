e70c5d74a7b31174a27ed48fef543256776b5ab9
Simon Richter <Simon.Richter@hogyros.de>
2012-12-29 09:34:28 +0000

    Output newline after returned data

--- vish.cpp
+++ vish.cpp
@@ -69,6 +69,7 @@ int main(int argc, char **argv)
                         else
                                 break;
                 }
+                std::cout << std::endl;
         }
 
         viClose(vi);
